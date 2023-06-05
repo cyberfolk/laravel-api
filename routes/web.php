164039ce09cc73ed275629509a5c3d75bdcda4f0
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-05 17:35:31 +0200

    Fic project into projects

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\Admin\DashboardController;
-use App\Http\Controllers\Admin\PostController;
+use App\Http\Controllers\Admin\ProjectController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -22,7 +22,7 @@ Route::get('/', function () {
 
 Route::middleware(['auth', 'verified'])->prefix('admin')->name('admin.')->group(function () {
     Route::get('/', [DashboardController::class, 'index'])->name('dashboard');
-    Route::resource('project', PostController::class)->parameters(['projects' => 'project:slug']);
+    Route::resource('projects', ProjectController::class)->parameters(['projects' => 'project:slug']);
 });
 
 Route::middleware('auth')->group(function () {
