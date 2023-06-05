bd5ae4e1a23169f6ffce3af35c70791a74296948
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-05 16:51:29 +0200

    Add import

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
+use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
