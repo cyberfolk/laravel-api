2b7cf7860f1f9aa18305c124c3a12184152a7582
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-07 16:13:11 +0200

    Fill TypeSeeder

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -3,7 +3,10 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
+use App\Models\Type;
+use Illuminate\Support\Str;
 
 class TypeSeeder extends Seeder
 {
@@ -12,8 +15,16 @@ class TypeSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        $base_type = ['front-end', 'back-end', 'full-stack'];
+
+        foreach ($base_type as $type) {
+            $newType = new Type();
+            $newType->name = $type;
+            $newType->slug = Str::slug($newType->name, '-');
+            $newType->link_cover = $faker->imageUrl(word: $type, randomize: false, format: 'jpg');
+            $newType->save();
+        }
     }
 }
