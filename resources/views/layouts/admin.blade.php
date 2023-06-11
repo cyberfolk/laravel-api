75ef23934b390ea535bdc5bc3a5579b8f2c9c252
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-12 00:15:03 +0200

    Create partials head and navbar

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,43 +1,11 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- CDN font-awesome -->
-    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css' integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==' crossorigin='anonymous' referrerpolicy='no-referrer' />
-
-    <!-- Usando Vite -->
-    @vite(['resources/js/app.js'])
-</head>
+@include('partials.head')
 
 <body>
     <div id="app">
-        <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap shadow">
-            <a class="navbar-brand col-md-3 col-lg-2 mr-0 px-3" href="#">LOGO</a>
-            <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-toggle="collapse" data-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-
-            <div class="dropdown px-3">
-                <button class="btn dropdown-toggle text-light" type="button" id="triggerId" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    {{ Auth::user()->name }}
-                </button>
-                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="triggerId">
-                    <a class="dropdown-item" href="{{ url('/') }}">{{ __('Home Page') }}</a>
-                    <a class="dropdown-item " href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
-                </div>
-                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                    @csrf
-                </form>
-            </div>
-        </nav>
+        @include('partials.navbar')
 
         <div class="container-fluid vh-100 mb-3">
             <div class="row h-100">
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-md navbar-dark sticky-top bg-dark shadow px-5">
+    <a class="navbar-brand d-flex align-items-center" href="{{ url('/') }}"> LOGO </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav me-auto">
+            <li class="nav-item"><a class="nav-link text-white" href="{{ url('/') }}">{{ __('Home') }}</a></li>
+            @auth
+                <li class="nav-item"><a class="nav-link text-white" href="{{ route('admin.dashboard') }}">{{ __('Dashboard') }}</a></li>
+            @endauth
+        </ul>
+
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            @guest
+                <li class="nav-item"><a class="nav-link text-white" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                <li class="nav-item"><a class="nav-link text-white" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+            @endguest
+            @auth
+                <li class="nav-item dropdown">
+                    <button id="navbarDropdown" class="nav-link text-white dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        {{ Auth::user()->name }}
+                    </button>
+                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('profile') }}">{{ __('Profile') }}</a>
+                        <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();"> {{ __('Logout') }} </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none"> @csrf </form>
+                    </div>
+                </li>
+            @endauth
+        </ul>
+    </div>
+</nav>
