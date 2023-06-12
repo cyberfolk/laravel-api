27d532e271ac4eaeea876e9d17c101995988b462
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-12 09:04:06 +0200

    Add edit and cretae

---
+++ resources/views/admin/types/create.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.admin')
+
+{{-- @section('page_title', 'Create') --}}
+
+@section('content')
+    <div class="container-fluid">
+
+        @include('partials.validation_errors')
+
+        <h5 class="text-uppercase text-muted my-3">Add a new type</h5>
+        <form action="{{ route('admin.types.store') }}" method="post">
+            @csrf
+
+            <div class="mb-3">
+                <label for="name" class="form-label">name</label>
+                <input type="text" required
+                    value="{{ old('name') }}" name="name" id="name"
+                    class="form-control @error('name') is-invalid @enderror"
+                    placeholder="type name here"
+                    aria-describedby="nameHelper">
+                <small id="nameHelper" class="text-secondary @error('name') text-danger @enderror">
+                    Type the name of the type max 50 characters
+                </small>
+            </div>
+            {{-- /.name --}}
+            <div class="mb-3">
+                <label for="link_cover" class="form-label">Cover</label>
+                <input type="text" required
+                    value="{{ old('link_cover') }}" name="link_cover" id="link_cover"
+                    class="form-control @error('link_cover') is-invalid @enderror"
+                    placeholder="type link_cover here"
+                    aria-describedby="link_coverHelper">
+                <small id="link_coverHelper"
+                    class="text-secondary @error('link_cover') text-danger @enderror">
+                    Type the source image of the type max 200 characters
+                </small>
+            </div>
+            {{-- /.link_cover --}}
+            <button type="submit" class="btn btn-dark w-100 my-4">Save</button>
+        </form>
+    </div>
+@endsection
