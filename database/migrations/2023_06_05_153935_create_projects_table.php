b78b11f30be3e211c97ce69cbe06a80c805394c5
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-13 23:30:32 +0200

    Change link_cover and dim title

--- database/migrations/2023_06_05_153935_create_projects_table.php
+++ database/migrations/2023_06_05_153935_create_projects_table.php
@@ -15,9 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
-            $table->string('title', 50)->unique();
+            $table->string('title', 100)->unique();
             $table->string('slug', 100);
-            $table->string('link_cover', 200);
+            $table->string('link_cover', 200)->nullable();
             $table->string('link_live', 200)->nullable();
             $table->string('link_code', 200);
             $table->text('description')->nullable(); //text not require dim
