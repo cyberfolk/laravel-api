b3a0b183a7fce0ef0310e5aa6be02a1f6700272d
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 17:37:38 +0200

    Set pagination on API

--- app/Http/Controllers/API/ProjectController.php
+++ app/Http/Controllers/API/ProjectController.php
@@ -10,7 +10,7 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::with(['type', 'technologies', 'user'])->orderByDesc('id')->get();
+        $projects = Project::with(['type', 'technologies', 'user'])->orderByDesc('id')->paginate(6);
         return response()->json([
             'succes' => true,
             'projects' => $projects,
