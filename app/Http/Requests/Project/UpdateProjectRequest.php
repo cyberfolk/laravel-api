b3f7cb7c0d38b39e466483085bb8be58edef90f7
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-14 09:16:05 +0200

    Split array with |

--- app/Http/Requests/Project/UpdateProjectRequest.php
+++ app/Http/Requests/Project/UpdateProjectRequest.php
@@ -26,12 +26,12 @@ class UpdateProjectRequest extends FormRequest
     {
         return [
             'title'       => ['required', Rule::unique('projects', 'title')->ignore($this->project), 'max:100'],
-            'start_date'  => ['required|date|before_or_equal:today|after:1900-01-01'],
-            'last_commit' => ['nullable|date|before_or_equal:today|after:sale_date'], //after:after:sale_date not work
+            'start_date'  => ['required', 'date', 'before_or_equal:today', 'after:1900-01-01'],
+            'last_commit' => ['nullable', 'date', 'before_or_equal:today', 'after:sale_date'], //after:after:sale_date not work
             'link_cover'  => ['nullable', 'image', 'max:1024'],
             'type_id'     => ['nullable', 'exists:types,id'],
-            'link_code'   => ['required|max:200'],
-            'link_live'   => ['nullable|max:200'],
+            'link_code'   => ['required', 'max:200'],
+            'link_live'   => ['nullable', 'max:200'],
             'code_line'   => ['nullable'],
             'folders'     => ['nullable'],
             'description' => ['nullable'],
