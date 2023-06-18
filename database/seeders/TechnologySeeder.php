b7e67645d45b94745006d220e370ad09825709c7
Andrea Ferrari <andrea1995ferrari@gmail.com>
2023-06-18 02:24:34 +0200

    Get and store svg from web

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Str;
 use Faker\Generator as Faker;
+use Illuminate\Support\Facades\Storage;
 
 class TechnologySeeder extends Seeder
 {
@@ -17,13 +18,51 @@ class TechnologySeeder extends Seeder
      */
     public function run(Faker $faker)
     {
-        $base_technologies = ['html', 'css', 'js', 'vue-js', 'saas', 'php', 'laravel', 'sql'];
 
-        foreach ($base_technologies as $technology) {
+        $technologies = [
+            'html' => [
+                'name' => 'html',
+                'icon' => 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
+            ],
+            'css' => [
+                'name' => 'css',
+                'icon' => 'https://www.vectorlogo.zone/logos/w3_css/w3_css-icon.svg',
+            ],
+            'js' => [
+                'name' => 'js',
+                'icon' => 'https://upload.vectorlogo.zone/logos/javascript/images/239ec8a4-163e-4792-83b6-3f6d96911757.svg',
+            ],
+            'vue-js' => [
+                'name' => 'vue-js',
+                'icon' => 'https://www.vectorlogo.zone/logos/vuejs/vuejs-icon.svg',
+            ],
+            'saas' => [
+                'name' => 'saas',
+                'icon' => 'https://www.vectorlogo.zone/logos/w3_html5/w3_html5-icon.svg',
+            ],
+            'php' => [
+                'name' => 'php',
+                'icon' => 'https://www.vectorlogo.zone/logos/sass-lang/sass-lang-icon.svg',
+            ],
+            'laravel' => [
+                'name' => 'laravel',
+                'icon' => 'https://www.vectorlogo.zone/logos/laravel/laravel-icon.svg',
+            ],
+            'sql' => [
+                'name' => 'sql',
+                'icon' => 'https://www.vectorlogo.zone/logos/mysql/mysql-icon.svg',
+            ],
+
+        ];
+
+        foreach ($technologies as $technology) {
             $newTechnology = new Technology();
-            $newTechnology->name = $technology;
+            $newTechnology->name = $technology['name'];
             $newTechnology->slug = Str::slug($newTechnology->name, '-');
-            $newTechnology->link_cover = $faker->imageUrl(word: $technology, randomize: false, format: 'jpg');
+            $dir = 'technologies/' . $technology['name'] . '.svg';
+            $contents = file_get_contents($technology['icon']);
+            Storage::put($dir, $contents);
+            $newTechnology->link_cover = $dir;
             $newTechnology->save();
         }
     }
